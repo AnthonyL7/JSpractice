88d33e12462acc834756bef10256f259f0b80dad
AnthonyL7 <anthonylanda022@gmail.com>
2024-12-25 15:31:51 -0800

    RockPaperScissors 8kyu

--- practice.js
+++ practice.js
@@ -1,11 +1,26 @@
-function between(a, b) {
-  let result = [];
-  for (let i = a; i<= b; i++) {
-    result.push(i)
+const rps = (p1, p2) => {
+  if (p1 == 'scissors' && p2 == 'paper') {
+    return 'Player 1 won!';
   }
-  return result
-  // your code here
-}
+  else if (p1 == 'scissors' && p2 == 'rock') {
+    return 'Player 2 won!';
+  }
+  else if (p1 == 'paper' && p2 == 'scissors') {
+    return 'Player 2 won!';
+  }
+  else if (p1 == 'paper' && p2 == 'rock' ) {
+    return 'Player 1 won!';
+  }
+  else if (p1 == 'rock' && p2 == 'scissors' ) {
+    return 'Player 1 won!';
+  }
+  else if (p1 == 'rock' && p2 == 'paper' ) {
+    return 'Player 2 won!';
+  }
+  else if (p1 === p2 ) {
+    return 'Draw!';
+  }
+};
+let result = rps('paper', 'scissors');
 
-let result = between(1,4);
-console.log(result);
+console.log(result)
