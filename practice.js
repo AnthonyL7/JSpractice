d2594f489c2174cbae34a1776d20f01af2989238
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-07 13:06:30 -0800

    10_fibonacci

--- practice.js
+++ practice.js
@@ -1,11 +1,14 @@
-// palindrome function
-function palindrome(str) {
-  const reverse = str.split('').reverse().join('')
-  if (str === reverse) {
-    return true
-  } else {
-    return false
-  }
+// fibonacci function
+function fibonacci(num) {
+  if (num <= 0) return [];
+   
+  if (num === 1) return [0];
+  
+  return Array(num).fill(0).map((_, i) => {
+    if (i === 0) return 0;
+    if (i === 1) return 1;
+    return fibonacci(i)[i - 1] + fibonacci(i)[i - 2];
+  });
 }
 
-console.log(palindrome('tacos'))
+console.log(fibonacci(10))
