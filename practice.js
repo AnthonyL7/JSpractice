9667b18b768e215d82880c9f943fc9245311661d
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-07 09:50:24 -0800

    Create Calculator functions

--- practice.js
+++ practice.js
@@ -1,12 +1,40 @@
-function isPalindrome(x) {
-  x = x.toLowerCase()
-  reversed = x.split('').reverse().join('')
-  if (x == reversed) {
-    return true
+function add(a,b) {
+  return a+b
+}
+
+console.log(add(2,2))
+
+function subtract(a,b) {
+  return a-b
+}
+
+console.log(subtract(4,2))
+
+sumArray = [1,2,3,4];
+
+const initialValue = 0
+const sumWithI = sumArray.reduce((total, initialValue) => total + initialValue);
+
+console.log(sumWithI)
+
+const multiply = sumArray.reduce((total, initialValue) => total * initialValue);
+
+console.log(multiply)
+
+function power(a,b) {
+  return Math.pow(a,b)
+}
+
+console.log(power(3,2))
+
+function factor(num) {
+  if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
   } else {
-    return false
+    return (num * factor(num-1))
   }
 }
 
-result = isPalindrome('AbBa')
-console.log(result)
+console.log(factor(5))
