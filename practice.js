36818b74e87637f70fcfbed8a8e4a5bd104caada
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-07 13:21:02 -0800

    11_getTheTitles in a function

--- practice.js
+++ practice.js
@@ -13,3 +13,20 @@ const books = [
 const arr = Array.from(books)
 const titles = arr.map(book => book.title)
 console.log(titles)
+
+
+function getTheTitles(titles) {
+  const arr = Array.from(books)
+  return arr.map(titles => titles.title)
+}
+
+console.log(getTheTitles([
+  {
+    title: "Can't Hurt Me",
+    author: 'David Goggins'
+  },
+  {
+    title: "Grit",
+    author: 'Angela Duckworth'
+  }
+]))
