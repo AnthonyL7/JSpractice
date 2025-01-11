4e573ad7734b686590402b5449ccbd8eedfadc9f
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-11 15:32:39 -0800

    Introduction to html tables

--- practice.js
+++ practice.js
@@ -1,39 +1,13 @@
-// Get the age
-const getAge = ({birthYear, deathYear}) =>  deathYear - birthYear;
-
-// Create arrow function
-const findTheOldest = function(people) {
-
-  // Assign accumAge to array index 0 of the arrayPeople list
-  let accumAge = getAge(people[0]);
-  return people.reduce((accum,curr) => {
-    let currAge = getAge(curr)
-    if (currAge > accumAge) {
-      accumAge = currAge;
-      return curr;
+function nbYear(p0, percent, aug, p) {
+  let num = 0
+  for (let i = 0; i < p; i++) {
+    if (p[i] < p) {
+      num += 1
+      return num
     }
-    return accum
-  })
+
+  }
 }
 
-
-const arrayPeople = [
-  {
-    birthYear: 1961,
-    deathYear: 2005
-  },
-  {
-    birthYear: 1955,
-    deathYear: 2025 
-  },
-  {
-
-    birthYear: 1923,
-    deathYear: 1999
-  },
-] 
-
-console.log(findTheOldest(arrayPeople))
-
-
-
+result = nbYear(1500, 5, 100, 5000)
+console.log(result)
