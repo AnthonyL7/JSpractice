fb045d88b9fb6dc6bbf7c5b1e4a00b498d6c4b87
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-12 11:54:25 -0800

    8 KYU Third Angle of a Triangle

--- practice.js
+++ practice.js
@@ -1,27 +1,9 @@
-/*function nbYear(p0, percent, aug, p) {
-  let num = 0;
-  for (let i = 0; i <= p; i++) {
-    if (((p0 + (p0 * (percent * 0.10))) + aug)) {
-      num ++;
-      return num;  
-    }
+function otherAngle(a,b) {
+  sum = a+b
+  if (a > 0 && b > 0) {
+    return 180 - sum 
   }
 }
 
-result = nbYear(1500, 5, 100, 5000)
+result = otherAngle(-30,60)
 console.log(result)
-*/
-
-function nbYear(p0, percent, aug, p) {
-  let num = 0;
-  let calculation = p0
-
-  while (calculation < p) {
-    num++;
-    p0 += Math.floor((p0 * (percent * 0.01)) + aug)
-    calculation = p0;
-  }
-  return num
-}
-
-console.log(nbYear(1000, 2.0, 50, 1214))
