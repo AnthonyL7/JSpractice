46b192c6a48e850d0824c841745636426d02aac6
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-12 12:44:25 -0800

    8 KYU if you cant sleep just count sheep

--- practice.js
+++ practice.js
@@ -1,9 +1,14 @@
-function otherAngle(a,b) {
-  sum = a+b
-  if (a > 0 && b > 0) {
-    return 180 - sum 
+
+function countSheep(num) {
+  let str = ''
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      str += i + " " + "sheep...";
+    }
+    return str;
+  } else {
+    return '';
   }
 }
 
-result = otherAngle(-30,60)
-console.log(result)
+console.log(countSheep(3))
