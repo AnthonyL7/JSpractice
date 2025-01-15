939cbb29a7348366b5973882488d533b792a5f2c
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-14 19:40:46 -0800

    7 Kyu New 5 pound notes collectors

--- practice.js
+++ practice.js
@@ -1,15 +1,15 @@
-function finalGrade(exam, projects) {
-  if (exam > 90 || projects > 10) {
-    return 100;
-  } else if (exam > 75 && projects >= 5)  {
-    return 90;
-  } else if (exam > 50 && projects >= 2) {
-    return 75;
+function getNewNotes(salary, bills) {
+  let sum = 0;
+  for (let i = 0; i < bills.length; i++) {
+    sum += bills[i];
+  }
+  afterExpenses = salary - sum;
+  if (afterExpenses >= 0 ) {
+    return Math.floor(afterExpenses / 5);
   } else {
     return 0;
   }
-
 }
 
-result = finalGrade(75,7)
+result =  getNewNotes(2300, [590, 1500, 45, 655, 150])
 console.log(result)
