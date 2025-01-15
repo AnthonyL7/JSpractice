358fcda5b4c20a87a72fe4eaffb949aecf16f0c2
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-14 18:48:15 -0800

    8 Kyu

--- practice.js
+++ practice.js
@@ -1,10 +1,15 @@
-function zeroFuel(distanceToPump, mpg, fuelLeft) {
-  if ((fuelLeft * mpg) >= distanceToPump) {
-    return true;
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5)  {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
   } else {
-    return false;
+    return 0;
   }
+
 }
 
-result = zeroFuel(50,25,2)
+result = finalGrade(75,7)
 console.log(result)
