9885145d0cdc6f38a7e9e6f9cc822ea211f41f4b
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-17 11:08:43 -0800

    8 Kyu

--- practice.js
+++ practice.js
@@ -1,9 +1,18 @@
-function isAnagram(a,b) {
- a = a.toLowerCase().split('').sort().join('');
- b = b.toLowerCase().split('').sort().join('');
-
- return a === b;
+function sumArray(array) {
+  let min = Math.min(...array)
+  let max = Math.max(...array)
+  let sum = 0;
+  console.log(min, max)
+  if (!Array.isArray(array) || array.length <= 1) {
+    return 0;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] != min && array[i] != max) {
+      sum = sum + array[i]
+    }
+  } 
+  return sum
 }
 
-let result =  isAnagram("Buckethead", "DeathCubeK")
+let result =  sumArray([3,5,7,10])
 console.log(result)
