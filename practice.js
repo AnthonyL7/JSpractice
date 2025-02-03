db6c95e72d28b0111ecf1ecede2c1ad4350e7b7b
AnthonyL7 <anthonylanda022@gmail.com>
2025-02-02 18:07:07 -0800

    8 Kyu Super Duper Easy

--- practice.js
+++ practice.js
@@ -1,6 +1,10 @@
-function removeUrlAnchor(url) {
-  return url.split('#')[0]
+function problem(x) {
+  if (typeof(x) === 'number') {
+    return (x * 50) + 6
+  } else {
+    return 'Error'
+  }
 }
 
-let result =  removeUrlAnchor('www.codewars.com#about')
+let result =  problem(5.5)
 console.log(result)
